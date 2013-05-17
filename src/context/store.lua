61a66b9c42bc37b9d81762a43c501d5343a877b6
Drew Ditthardt <dditthardt@olivinelabs.com>
2013-05-17 08:49:51 -0700

    moved collection into code instead of string parameter

--- src/context/store.lua
+++ src/context/store.lua
@@ -1,19 +1,43 @@
-local store = function(method)
-  return function(collection, query, data)
-    return context.lusty:publish(
+local store = function(collection, method)
+  return function(query, data)
+    local result = context.lusty:publish(
     { 'store', collection, method },
     { method=method, query = query, data = data, collection=collection }
     )
+    local ret = {}
+    if #result > 0 then
+      for i=1, #result do
+        local row = result[i]
+        if row and type(row) == 'table' and #row > 0 then
+          for j=1, #row do
+            ret[#ret+1] = row[j]
+          end
+        else
+          ret[#ret+1] = row
+        end
+      end
+    end
+    if #ret == 1 then ret = ret[1] end
+    return ret
   end
 end
 
 context.store = setmetatable({}, {
-  __index = function(self, key)
-    local val = rawget(self, key)
-    if not val then
-      val = store(key)
-      self[key] = val
+  __index = function(self, collection)
+    local collectionTable = rawget(self, collection)
+    if not collectionTable then
+      collectionTable = setmetatable({}, {
+        __index = function(self, method)
+          local methodFunction = rawget(self, method)
+          if not methodFunction then
+            methodFunction = store(collection, method)
+            self[method] = methodFunction
+          end
+          return methodFunction
+        end
+      })
+      self[collection] = collectionTable
     end
-    return val
+    return collectionTable
   end
 })
